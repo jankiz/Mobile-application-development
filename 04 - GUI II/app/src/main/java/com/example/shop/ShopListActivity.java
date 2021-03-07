5adfc09d0817de4000b431a2d60597009c3a6a86
Jánki Zoltán R <jankiz@inf.u-szeged.hu>
2021-03-07 17:26:26 +0100

    4th practice source code readded.

---
+++ 04 - GUI II/app/src/main/java/com/example/shop/ShopListActivity.java
@@ -0,0 +1,14 @@
+package com.example.shop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShopListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shop_list);
+    }
+}
